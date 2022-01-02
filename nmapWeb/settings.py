6d349cc0cf2759591be12be6f835054655fcd489
chapman808 <anthony.n.chapman@gmail.com>
2022-01-02 15:34:41 -0800

    reset db creds

--- nmapWeb/settings.py
+++ nmapWeb/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nmap',
         'USER': 'admin',
-        'PASSWORD': 'abcd1234',
-        'HOST': 'nmap-online.ci3q4emuhlqv.us-west-2.rds.amazonaws.com',
+#       'PASSWORD': 'CHANGEME',
+#        'HOST': 'CHANGEME',
         'PORT': '3306',
     }
 }
