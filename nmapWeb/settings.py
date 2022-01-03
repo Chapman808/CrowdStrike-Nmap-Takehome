72241cb3cc0fcfec7b4f15f3a9ee03b2a002a895
chapman808 <anthony.n.chapman@gmail.com>
2022-01-03 15:17:52 -0800

    turning back on debug mode-- we will disable this in prod

--- nmapWeb/settings.py
+++ nmapWeb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w(o7u!q^g-%k#n*%su!nk7y-t-9+m=krfk*ynj23c^0=z&=ji$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
